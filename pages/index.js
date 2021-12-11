425e2f54aace620ca6837066363364f170c401ca
Narottam04 <coutkunal@gmail.com>
2021-12-11 14:32:22 +0530

    possible fix for hosting

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
   }
 
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
+    <div className="flex flex-col items-center justify-center h-screen">
       <Head>
         <title>Google Clone</title>
         <meta name="description" content="Google Clone Using Nextjs" />
@@ -30,42 +30,42 @@ export default function Home() {
       </Head>
 
       {/* header */}
-      <header className='flex w-full p-5 justify-between text-sm text-gray-700'>
+      <header className="flex w-full p-5 justify-between text-sm text-gray-700">
 
         {/* left */}
         <div className="flex space-x-4 items-center">
-          <p className='link'>About</p>
-          <p className='link'>Store</p>
+          <p className="link">About</p>
+          <p className="link">Store</p>
         </div>
 
         {/* right */}
         <div className="flex space-x-4 items-center">
-          <p className='link'>Gmail</p>
-          <p className='link'>Images</p>
+          <p className="link">Gmail</p>
+          <p className="link">Images</p>
           {/* icon */}
-          <ViewGridIcon className='h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer'/>
+          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer"/>
           
           <Avatar url="/Narottam-Profile.jpeg"/>
         </div>
       </header>
 
       {/* body */}
-      <form className='flex flex-col items-center mt-44 flex-grow w-4/5' >
+      <form className="flex flex-col items-center mt-44 flex-grow w-4/5" >
         <Image
-          src='/google.jpg'
+          src="/google.jpg"
           height={95}
           width={304}
-          alt='google image'
+          alt="google image"
         />
-        <div className='flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl'>
-          <SearchIcon className='h-5 mr-3 text-gray-500' />
-          <input ref={searchInputRef} type="text" className='focus:outline-none flex-grow'/>
-          <MicrophoneIcon className='h-5' />
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input ref={searchInputRef} type="text" className="focus:outline-none flex-grow"/>
+          <MicrophoneIcon className="h-5" />
         </div>
 
-        <div className='flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4'>
-          <button onClick={search}  className='btn'>Google Search</button>
-          <button onClick={search} className='btn'>I'm Feeling Lucky</button>
+        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+          <button onClick={search}  className="btn">Google Search</button>
+          <button onClick={search} className="btn">I'm Feeling Lucky</button>
         </div>
       </form>
       {/* footer */}
